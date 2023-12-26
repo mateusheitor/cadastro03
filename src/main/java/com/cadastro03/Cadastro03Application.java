af3de6644947d3e538befb540669010682bffa17
mateusheitor <mateusheitoroficial@gmail.com>
2023-12-26 10:47:40 -0300

    first commit

---
+++ src/main/java/com/cadastro03/Cadastro03Application.java
@@ -0,0 +1,13 @@
+package com.cadastro03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cadastro03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cadastro03Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/cadastro03/model/Pessoa.java
@@ -0,0 +1,57 @@
+package com.cadastro03.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Pessoa {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column
+	private String nome;
+
+	@Column
+	private String email;
+
+	@Column
+	private String telefone;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+}
