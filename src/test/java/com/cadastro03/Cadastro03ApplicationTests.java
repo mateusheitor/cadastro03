1d832b1de0a7c3e0574ebce9503b959cd6deaa7c
mateusheitor <mateusheitoroficial@gmail.com>
2023-12-26 11:25:16 -0300

    adicionando primeiro arquivo

---
+++ src/test/java/com/cadastro03/Cadastro03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadastro03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cadastro03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
